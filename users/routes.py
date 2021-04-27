36550a17d38df02cd3c47f7aa9a52d7432851726
Шихалёв Денис <shihalev.denis@micard.ru>
2021-04-27 18:26:23 +0300

    Added hash for password and exception handling

--- users/routes.py
+++ users/routes.py
@@ -1,28 +1,48 @@
 from sanic.response import json
 from users.tables import users
+from passlib.hash import sha256_crypt
+from users.utils import generate_token
+from sanic.exceptions import Unauthorized, InvalidUsage
 
 
 def setup_routes(app):
     @app.post("/user/registry")
     async def register_user(request):
         payload = request.json
-        username = payload['username']
-        email = payload['email']
-        password = payload['password']
+        try:
+            username = payload['username']
+            email = payload['email']
+            password = payload['password']
+        except KeyError:
+            raise InvalidUsage("Invalid message format")
+
         query = users.insert().values(
             username=username,
             email=email,
-            password=password
+            password=sha256_crypt.hash(password)
         )
         await request.app.ctx.db.execute(query)
         return json({}, status=201)
-    #
-    # @app.route("/user/auth")
-    # async def auth_user(request):
-    #     query = users.select()
-    #     rows = await request.app.ctx.db.fetch_all(query)
-    #     return json({})
-    #
+
+    @app.post("/user/auth")
+    async def auth_user(request):
+        payload = request.json
+        try:
+            username = payload['username']
+            password = payload['password']
+        except KeyError:
+            raise InvalidUsage("Invalid message format")
+        query = users.select([users.c.id, users.c.password]).where(users.c.username == username)
+        row = await request.app.ctx.db.fetch_one(query)
+        if row is None:
+            raise Unauthorized("Invalid credentials")
+        user_id, hash_pass = row[0], row[1]
+        if sha256_crypt.verify(password, hash_pass):
+            token = await generate_token(user_id)
+        else:
+            raise Unauthorized("Invalid credentials")
+        return json({'user_id': user_id, 'token': token})
+
     # @app.route("/user/{user_id}")
     # async def get_user(request):
     #     query = users.select()
@@ -33,4 +53,4 @@ def setup_routes(app):
     async def test(request):
         query = users.select()
         rows = await request.app.ctx.db.fetch_all(query)
-        return json({})
+        return json([{'id': row[0], 'username': row[1], 'email': row[2], 'password': row[3]} for row in rows])
---
+++ users/utils.py
@@ -0,0 +1,12 @@
+import jwt
+from datetime import datetime, timedelta
+
+JWT_SECRET = 'secret'
+JWT_ALGORITHM = 'HS256'
+JWT_EXPIRATION_SEC = 60
+
+
+async def generate_token(user_id):
+    expiration_time = datetime.utcnow() + timedelta(JWT_EXPIRATION_SEC)
+    token = jwt.encode({'user_id': user_id, 'exp_time': expiration_time})
+    return token.decode('utf-8')
